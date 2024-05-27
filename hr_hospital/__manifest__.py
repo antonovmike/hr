21374df5f4a7302bbc33e8e3431cc8fa0e331679
antonovmike <antonovma@outlook.com>
2024-05-27 08:25:25 +0400

    first

---
+++ hr_hospital/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "HR Hospital",
+    'version': '16.0.1.0.0',
+    'category': 'Uncategorized',
+    'summary': """Odoo test task""",
+    'license': 'LGPL-3',
+    'author': "Antonov Mike",
+    'website': 'https://github.com/antonovmike',
+    'depends': [
+        'base', 
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'demo': [],
+    'images': [],
+    'live_test_url': '',
+    'price': 0,
+    'currency': 'EUR',
+    'support': '',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
---
+++ hr_hospital/models/__init__.py
@@ -0,0 +1,1 @@
+from . import models, doctors, patients, types_of_diseases, visits_of_patients
