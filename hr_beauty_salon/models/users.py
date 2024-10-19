8effe1e9983187db9022ed7d08c5aa9bfab4699c
antonovmike <antonovma@outlook.com>
2024-10-19 08:03:53 +0200

    [IMP] Created basic User model and view

--- hr_beauty_salon/models/users.py
+++ hr_beauty_salon/models/users.py
@@ -1,8 +1,9 @@
-from odoo import models
+from odoo import models, fields
 
 
 class User(models.Model):
     _name = 'hr_beauty_salon.user'
     _description = 'User'
 
+    name = fields.Char(string="Name", required=True, default="Jane Doe")
     # pass
